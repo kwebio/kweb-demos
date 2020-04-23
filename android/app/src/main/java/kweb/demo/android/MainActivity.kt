ab2330786bb70c0157bb532b02f9c78553137dd1
Ethan Davidson <ethanmdavidson@gmail.com>
2020-04-22 21:39:03 -0400

    android demo initial commit

---
+++ android/app/src/main/java/kweb/demo/android/MainActivity.kt
@@ -0,0 +1,26 @@
+package kweb.demo.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kweb.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+
+        Kweb(port = 16097, buildPage = {
+            doc.body.new {
+                h1().text("Hello World!")
+            }
+        })
+
+        val webView: WebView = findViewById(R.id.webViewWidget)
+
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("http://localhost:16097")
+    }
+}
